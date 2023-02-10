shaprann/spatiotemporal

commit 3460ddfed42875066c9305fa5c1cd968f310c27a

Bug fix: do not load to device inside __getitem__()
This prevents PyTorch from creating processes. Instead use pin_memory=True in DataLoader and non_blocking=True in .to() in the training loop.

ctgan_dataset.py
@@ -67,18 +67,19 @@ class CTGANTorchDataset(Dataset):
         return {
             "index":                index,
             "original_s2_image":    original_s2_image,
-            "target_image":         torch.from_numpy(target_image).to(self.device),
+            "target_image":         torch.from_numpy(target_image),
             "inputs":               [
-                                        torch.from_numpy(s2_image).to(self.device)
+                                        torch.from_numpy(s2_image)
                                         for s2_image in input_images
                                     ],
             "input_cloud_maps":     [
-                                        torch.from_numpy(cloud_map).to(self.device)
+                                        torch.from_numpy(cloud_map)
                                         for cloud_map in input_cloud_maps
                                     ],
             "cloud_percentage":     cloud_percentage
         }
 
+    @staticmethod
     def collate_fn(self, list_of_samples):
 
         result = {

commit 40e7b45b7875b4439606668c76486d21cba1efdc

Bug fix: collate function would not work normally due to a change introduced in commit 3460ddfe

ctgan_dataset.py
@@ -90,7 +90,7 @@ class CTGANTorchDataset(Dataset):
         }
 
     @staticmethod
-    def collate_fn(self, list_of_samples):
+    def collate_fn(list_of_samples):
 
         result = {
             "index": [],