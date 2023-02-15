dfdd29441029f1b96f359a5f23672111f074f0f0
vlad <shapran@daad-alumni.de>
2023-02-15 22:08:17 +0100

    Add cGAN dataset for PLFM

--- plfm_dataset.py
+++ plfm_dataset.py
@@ -116,6 +116,108 @@ class PLFM_LSTM_TorchDataset(Dataset):
         return result
 
 
+class PLFM_cGAN_TorchDataset(Dataset):
+
+    bands = [3, 2, 1, 7]  # [red, green, blue, NIR]
+
+    def __init__(self, dataset_manager, mode=None):
+
+        self._check_init_arguments(dataset_manager, mode)
+        self.manager = dataset_manager
+        self.mode = mode
+        self.data = self.manager.data_subset(split=mode).copy()
+
+        # copy some function from manager for better code readability
+        self.read_tif = self.manager.utils.read_tif
+        self.rescale_s2 = self.manager.utils.rescale_s2
+        self.rescale_s2_back = self.manager.utils.rescale_s2_back
+        self.rescale_s1 = self.manager.utils.rescale_s1
+        self.rescale_s1_back = self.manager.utils.rescale_s1_back
+        self.add_speckle = self.manager.utils.add_speckle
+        self.get_cloud_map = self.manager.utils.get_cloud_map
+
+    @staticmethod
+    def _check_init_arguments(dataset_manager, mode):
+
+        if not dataset_manager.has_cloud_maps:
+            raise ValueError("Provided dataset manager does not contain paths to cloud maps. "
+                             "Check whether path to cloud maps directory has been provided to dataset manager.")
+
+        if mode is not None and mode not in ["train", "test", "val"]:
+            raise ValueError(f"Incorrect mode provided. "
+                             f"Supported modes: None, 'train', 'test', 'val'. Got instead: {mode}")
+
+    def sneak_peek(self, idx):
+        """
+        Allows to take a sneak peek at cloud percentage at specific index.
+        This can reduce computation because batch can be discarded without loading the whole sample
+
+        :param idx: same idx as in __getitem__()
+        :return: cloud percentage
+        """
+        index = self.data.index[idx]
+        cloud_map = self.get_cloud_map(index=index)
+        return cloud_map.mean()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+
+        sample = self.data.iloc[idx]
+        index = sample.name
+
+        original_s2_image = self.read_tif(sample["S2"])
+        original_s1_image = self.read_tif(sample["S1"])
+        cloud_map = self.get_cloud_map(
+            cloud_map_path=sample["S2CLOUDMAP"],
+            s2_image=original_s2_image
+        )
+        cloud_percentage = cloud_map.mean()
+
+        target_s2_image = self.rescale_s2(original_s2_image)[self.bands]
+        input_s1_image = self.rescale_s1(original_s1_image)
+
+        fake_s1_image = self.rescale_s1_back(self.rescale_s2(original_s2_image)[[3, 2]])
+        fake_s1_image[0] = self.add_speckle(fake_s1_image[0])
+        fake_s1_image[1] = self.add_speckle(fake_s1_image[1])
+        fake_s1_image = self.rescale_s1(fake_s1_image)
+
+        return {
+            "index":                index,
+            "original_s2_image":    original_s2_image,
+            "target_s2_image":      torch.from_numpy(target_s2_image),
+            "input_s1_image":       torch.from_numpy(input_s1_image),
+            "fake_s1_image":        torch.from_numpy(fake_s1_image),
+            "cloud_percentage":     cloud_percentage
+        }
+
+    @staticmethod
+    def collate_fn(list_of_samples):
+
+        result = {
+            "index": [],
+            "original_s2_image": [],
+            "target_s2_image": [],
+            "input_s1_image": [],
+            "fake_s1_image": [],
+            "cloud_percentage": []
+        }
+        for sample in list_of_samples:
+            result["index"].append(sample["index"])
+            result["original_s2_image"].append(sample["original_s2_image"])
+            result["target_s2_image"].append(sample["target_s2_image"])
+            result["input_s1_image"].append(sample["input_s1_image"])
+            result["fake_s1_image"].append(sample["fake_s1_image"])
+            result["cloud_percentage"].append(sample["cloud_percentage"])
+
+        result["target_s2_image"] = torch.stack(result["target_s2_image"])
+        result["input_s1_image"] = torch.stack(result["input_s1_image"])
+        result["fake_s1_image"] = torch.stack(result["fake_s1_image"])
+
+        return result
+
+
 class PLFM_LSTM_TorchIterableDataset(IterableDataset):
 
     def __init__(
