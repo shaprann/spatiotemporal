f2565d63ec782f07515974fb40d0952d715a5063
vlad <shapran@daad-alumni.de>
2023-02-16 06:28:14 +0100

    Add UNET PLFM dataset

--- plfm_dataset.py
+++ plfm_dataset.py
@@ -4,6 +4,131 @@ import numpy as np
 import math
 
 
+class PLFM_UNET_TorchDataset(Dataset):
+
+    bands = [3, 2, 1, 7]  # [red, green, blue, NIR]
+
+    def __init__(self, dataset_manager, mode=None):
+
+        self._check_init_arguments(dataset_manager, mode)
+        self.manager = dataset_manager
+        self.mode = mode
+        self.data = self.manager.data_subset(split=mode).copy()
+        self._prepare_data()
+
+        # copy some function from manager for better code readability
+        self.read_tif = self.manager.utils.read_tif
+        self.rescale_s2 = self.manager.utils.rescale_s2
+        self.rescale_s2_back = self.manager.utils.rescale_s2_back
+        self.rescale_s1 = self.manager.utils.rescale_s1
+        self.rescale_s1_back = self.manager.utils.rescale_s1_back
+        self.add_speckle = self.manager.utils.add_speckle
+        self.get_cloud_map = self.manager.utils.get_cloud_map
+        self.fillnan = self.manager.utils.fillnan
+
+    @staticmethod
+    def _check_init_arguments(dataset_manager, mode):
+
+        if mode is not None and mode not in ["train", "test", "val"]:
+            raise ValueError(f"Incorrect mode provided. "
+                             f"Supported modes: None, 'train', 'test', 'val'. Got instead: {mode}")
+
+    def _prepare_data(self):
+
+        self.data = self.data.drop("S1", axis=1)
+
+        self.data["S2_t-1"] = self.data["S2"].groupby(level=["ROI", "tile", "patch"]).shift(1)
+        self.data["S2_t-2"] = self.data["S2"].groupby(level=["ROI", "tile", "patch"]).shift(2)
+        self.data["S2_t-3"] = self.data["S2"].groupby(level=["ROI", "tile", "patch"]).shift(3)
+        self.data["S2_t-4"] = self.data["S2"].groupby(level=["ROI", "tile", "patch"]).shift(4)
+        self.data["S2_t-5"] = self.data["S2"].groupby(level=["ROI", "tile", "patch"]).shift(5)
+
+        self.data = self.data.dropna(how="any")
+
+    def sneak_peek(self, idx):
+        """
+        Allows to take a sneak peek at cloud percentage at specific index.
+        This can reduce computation because batch can be discarded without loading the whole sample
+
+        :param idx: same idx as in __getitem__()
+        :return: cloud percentage
+        """
+        index = self.data.index[idx]
+        cloud_map = self.get_cloud_map(index=index)
+        return cloud_map.mean()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+
+        sample = self.data.iloc[idx]
+        index = sample.name
+
+        original_s2_image = self.read_tif(sample["S2"])
+
+        target_image = self.rescale_s2(original_s2_image)[self.bands]
+
+        # need this for cloud maps
+        input_images = [
+            self.read_tif(sample["S2_t-5"]),
+            self.read_tif(sample["S2_t-4"]),
+            self.read_tif(sample["S2_t-3"]),
+            self.read_tif(sample["S2_t-2"]),
+            self.read_tif(sample["S2_t-1"])
+        ]
+
+        input_images = [
+            self.rescale_s2(image)[self.bands] for image in input_images
+        ]
+
+        cloud_percentage = self.get_cloud_map(
+            cloud_map_path=sample["S2CLOUDMAP"],
+            s2_image=original_s2_image
+        ).mean()
+
+        original_s1_image = self.fillnan(self.read_tif(sample["S1"]))
+
+        input_s1_image = self.rescale_s1(original_s1_image)
+
+        return {
+            "index":                index,
+            "original_s2_image":    original_s2_image,
+            "target_image":         torch.from_numpy(target_image),
+            "input_s1_image":       torch.from_numpy(input_s1_image),
+            "inputs":               [
+                                        torch.from_numpy(s2_image)
+                                        for s2_image in input_images
+                                    ],
+            "cloud_percentage":     cloud_percentage
+        }
+
+    @staticmethod
+    def collate_fn(list_of_samples):
+
+        result = {
+            "index": [],
+            "original_s2_image": [],
+            "target_image": [],
+            "input_s1_image": [],
+            "inputs": [],
+            "cloud_percentage": []
+        }
+        for sample in list_of_samples:
+            result["index"].append(sample["index"])
+            result["original_s2_image"].append(sample["original_s2_image"])
+            result["target_image"].append(sample["target_image"])
+            result["input_s1_image"].append(sample["input_s1_image"])
+            result["inputs"].append(torch.stack(sample["inputs"]))
+            result["cloud_percentage"].append(sample["cloud_percentage"])
+
+        result["target_image"] = torch.stack(result["target_image"])
+        result["input_s1_image"] = torch.stack(result["input_s1_image"])
+        result["inputs"] = torch.stack(result["inputs"])
+
+        return result
+
+
 class PLFM_LSTM_TorchDataset(Dataset):
 
     bands = [3, 2, 1, 7]  # [red, green, blue, NIR]
